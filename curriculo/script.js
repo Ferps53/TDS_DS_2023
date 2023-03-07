9033caaf6efd30b35d7bd587a98003d911429692
Ferps53 <felipe.brostolin53@gmail.com>
2023-03-07 08:39:16 -0300

    Alterações para o Menu Dropdown

---
+++ curriculo/script.js
@@ -0,0 +1,12 @@
+function menuDisplay(){
+    var checkBox = document.getElementById("chkMenu");
+    var nav = document.getElementById("navigation");
+
+    if (checkBox.checked == true){
+        nav.style.display = "grid";
+    }
+    else{
+        nav.style.display = "none";
+    }
+}
+
