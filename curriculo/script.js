b33d150aa4b02d26b7ae6172da283240011af6f0
Ferps53 <felipe.brostolin53@gmail.com>
2023-03-09 20:07:55 -0300

    Finalização do trabalho

--- curriculo/script.js
+++ curriculo/script.js
@@ -1,11 +1,11 @@
-function menuDisplay(){
+function menuDisplay() {
     var checkBox = document.getElementById("chkMenu");
     var nav = document.getElementById("navigation");
 
-    if (checkBox.checked == true){
+    if (checkBox.checked == true) {
         nav.style.display = "grid";
     }
-    else{
+    else {
         nav.style.display = "none";
     }
 }
