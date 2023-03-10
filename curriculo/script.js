78c1f896204a0029b412f501a433e053afcf2be4
Ferps53 <felipe.brostolin53@gmail.com>
2023-03-09 22:29:54 -0300

    começo do verdadeiro curriculo

--- curriculo/script.js
+++ curriculo/script.js
@@ -10,3 +10,47 @@ function menuDisplay() {
     }
 }
 
+
+function plus(){
+    var numPage = 1;
+    var plusButton = document.getElementById("plus");
+    if(plusButton.click){
+        numPage++;
+        alert(numPage);
+    }
+}
+
+
+function carrosel(){
+    var numPage = 1;
+    var plusButton = document.getElementById("plus");
+    var minusButton = document.getElementById("minus");
+
+   
+    if(minusButton.click){
+        numPage--;
+    }
+    if(numPage < 1){
+        numPage = 4;
+    }else if(numPage > 4){
+        numPage = 1
+    }
+
+    switch(numPage){
+        case 1:
+            //display page 1
+            break;
+        case 2:
+            //display page 2
+            break;
+        case 3:
+            //display page 3
+            break;
+        case 4:
+            //display page 4
+            break;
+    }
+
+
+}
+
