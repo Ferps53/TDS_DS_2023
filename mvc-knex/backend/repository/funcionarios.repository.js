f3172bb5462217319d987bfdd25d5ae2e5e4d066
Ferps53 <felipe.brostolin53@gmail.com>
2023-06-14 18:58:44 -0300

    começando o desafio hbs

--- mvc-knex/backend/repository/funcionarios.repository.js
+++ mvc-knex/backend/repository/funcionarios.repository.js
@@ -2,19 +2,19 @@ const connection = require("../mysql-connection.js")
 
 module.exports = {
     find: () =>{
-        return connection.select().from("FUNCIONARIOS")
+        return connection.select().from("funcionarios")
     },
     findId: (id) =>{
-        return connection.where("id", "=", id).select().from("FUNCIONARIOS")
+        return connection.where("id", "=", id).select().from("funcionarios")
     },
     insert: (data) =>{
-        return connection.insert(data).into("FUNCIONARIOS")
+        return connection.insert(data).into("funcionarios")
     },
     update: (data, id) =>{
-        return connection("FUNCIONARIOS").update(data).where("id", "=", id)
+        return connection("funcionarios").update(data).where("id", "=", id)
     },
     delete: (id)=>{
-        return connection("FUNCIONARIOS").del().where("id", "=", id)
+        return connection("funcionarios").del().where("id", "=", id)
     }
     
 }
